1058853e44160e186501e08c3de604ef3429d09e
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-07-25 00:45:11 +0300

    feat: implement config file (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
+import { jobs } from './config/config'
 import { BackupAggregator } from './services/BackupAggregator'
 import { FileAggregator } from './services/FileAggregator'
-import { BackupJob } from './types/types'
-
-export const jobs: BackupJob[] = [
-  {
-    uri: '',
-    dbName: 'Garaaz24',
-    collections: ['listings', 'organizations'],
-  },
-  {
-    uri: '/',
-    dbName: 'Garaaz24',
-    collections: ['users'],
-  },
-]
 
 const fileAggregator = new FileAggregator('C:\\backuppy')
 const backupAggregator = new BackupAggregator(jobs, fileAggregator)
