8e26eff64408c6d2f1170a4197308d10af1c5254
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-07-25 00:14:42 +0300

    feat: implement backing up db to file & zipping it

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-console.log('hello')
-const test: string = 'test'
+import { BackupAggregator } from './services/BackupAggregator'
+import { FileAggregator } from './services/FileAggregator'
+import { BackupJob } from './types/types'
+
+export const jobs: BackupJob[] = [
+  {
+    uri: '',
+    dbName: 'Garaaz24',
+    collections: ['listings', 'organizations'],
+  },
+  {
+    uri: '/',
+    dbName: 'Garaaz24',
+    collections: ['users'],
+  },
+]
+
+const fileAggregator = new FileAggregator('C:\\backuppy')
+const backupAggregator = new BackupAggregator(jobs, fileAggregator)
---
+++ src/services/FileAggregator.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs'
+import JSZip from 'jszip'
+import { zipFolder } from '../util'
+
+export class FileAggregator {
+  private temp_path: string
+  private last_archive_path: string | null
+
+  constructor(backup_path: string) {
+    this.temp_path = backup_path
+    this.last_archive_path = null
+  }
+
+  saveObject = (obj: any, col: string, dbName: string) => {
+    const ISODate = new Date().toISOString().replaceAll(':', '-')
+    const dbPath = `${this.temp_path}\\result\\${dbName}`
+
+    if (!fs.existsSync(dbPath)) {
+      fs.mkdirSync(dbPath, { recursive: true })
+    }
+
+    fs.writeFileSync(`${dbPath}\\[${ISODate}] ${col}.json`, JSON.stringify(obj, null, 2))
+  }
+
+  prepareBackup = async () => {
+    const ISODate = new Date().toISOString().replaceAll(':', '-')
+    const archivePath = `${this.temp_path}\\${ISODate}-bak.zip`
+
+    await zipFolder(`${this.temp_path}\\result`, archivePath)
+    this.last_archive_path = archivePath
+
+    return archivePath
+  }
+
+  cleanUp = () => {
+    fs.rmSync(`${this.temp_path}\\result\\`, { recursive: true })
+
+    if (this.last_archive_path) {
+      fs.rmSync(this.last_archive_path)
+    }
+  }
+}
